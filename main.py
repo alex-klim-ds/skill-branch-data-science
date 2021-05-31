69cf89b00a042e87604fadfee187a0e4c121eb01
Alexander Klimenko <56605697+alex-klim-ds@users.noreply.github.com>
2021-05-31 13:36:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 """1: посчитать значение производной функции
 #$\cos(x) + 0.05x^3 + \log_2{x^2}$ в точке $x = 10$."""
 def function_1(x):
-    return round(np.cos(x) + 0.05 * x**3 + \
-        np.log2(x**2), 2)
+    return np.cos(x) + 0.05 * x**3 + \
+        np.log2(x**2)
 def derivation(x):#производная от function_1(x)
     return - np.sin(x) + 0.15 * x**2 + \
            (2 / (x * np.log(2)))
@@ -13,11 +13,11 @@ print('1: Значение производной' +' '+ str(round(derivation(10
 """2: посчитать значение градиента функции
 $x_1^2\cos(x_2) + 0.05x_2^3 + 3x_1^3"""
 def function_2(x1, x2):
-    return round(x1**2 * np.cos(x2) + 0.05 * x2**3 + \
-           3 * x1**3 * np.log2(x2**2), 2)
+    return x1**2 * np.cos(x2) + 0.05 * x2**3 + \
+           3 * x1**3 * np.log2(x2**2)
 def d_dx1(x1, x2):#частная производная по x1
-    return round(9*x1**2 * np.log2(x2**2) + \
-                 2 * x1 * np.cos(x2), 2)
+    return 9*x1**2 * np.log2(x2**2) + \
+                 2 * x1 * np.cos(x2)
 def d_dx2(x1, x2):#частная производная по x2
     return round(0.15 * x1**2 +\
                  ((6 * x1**3) / np.log(2) * (x2)) - \
@@ -25,4 +25,4 @@ def d_dx2(x1, x2):#частная производная по x2
 def gradient(x1, x2):
     return ([d_dx1(x1, x2), d_dx2(x1, x2)])
 
-print('2: Градиент функции:' +' '+ str(gradient(10, 1)))
+print('2: Градиент функции:' +' '+ str(round(gradient(10, 1), 2)))
